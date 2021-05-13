690777aff800da33acfb7d14ad3694bb4f52ea21
jeonjunhyeon <friendwnsgus@naver.com>
2021-05-13 17:05:13 +0900

    의존관계의 aop, expression를 lib폴더 내에 추가

--- spring4_1_1/src/kosmo80/mvc/sp4/DeptController.java
+++ spring4_1_1/src/kosmo80/mvc/sp4/DeptController.java
@@ -1,5 +1,11 @@
 package kosmo80.mvc.sp4;
 
-public class DeptController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public class DeptController extends MultiActionController{
+	public ModelAndView deptList(HttpServletRequest req, HttpServletResponse res) {
+		ModelAndView mav = new ModelAndView();
+		return mav;
+	}
 }
