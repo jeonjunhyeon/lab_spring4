253286cdc5efc57da204779f343a4057a7a5b0db
jeonjunhyeon <friendwnsgus@naver.com>
2021-05-14 17:34:15 +0900

    EmpController과 EmpLogic 잘못된 부분이 있어서 수정 후 업로드

--- spring4_1_1/src/kosmo80/mvc/sp4/EmpController.java
+++ spring4_1_1/src/kosmo80/mvc/sp4/EmpController.java
@@ -1,6 +1,6 @@
 package kosmo80.mvc.sp4;
 
-
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,28 +9,37 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 public class EmpController extends MultiActionController {
 
-		public ModelAndView getEmpList(HttpServletRequest req
-										, HttpServletResponse res) {
-			Logger logger = Logger.getLogger(EmpController.class);
-			private EmpLogic empLogic = null;
-			//setter 객체 주입 코드
-			
-			 List<Map<String, Object>> empList = new ArrayList<>();
-			 Map<String, Object> rmap = new HashMap<>();
-			 rmap.put()
-			ModelAndView mav = new ModelAndView();                    
-			/*
-			 * <property name="prefix" value="/WEB-INF/views/"/> <property name="prefix"
-			 * value=".jsp/"/>
-			 */
-			mav.setViewName("di/getEmpList");
-			return mav;
-		}
-			
+	public ModelAndView getEmpList(HttpServletRequest req, HttpServletResponse res) {
+		ModelAndView				mav		= new ModelAndView();
+		List<Map<String, Object>>	empList	= new ArrayList<>();
+		Map<String, Object>			rmap	= new HashMap<>();
+
+		rmap.put("mem_name", "이순신");
+		empList.add(rmap);
+//		ModelAndView는 scope 속성이 request이다.
+		mav.addObject("empList", empList);
+
+//		<property name="prefix" value="/WEB-INF/views/" />
+//		<property name="suffix" value=".jsp" />
+//		/WEB-INF/views/getEmpList.jsp
+		mav.setViewName("di/getEmpList");
+		return mav;
+	}
+
+	public void empInsert(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		res.sendRedirect("di/empInsert");
+	}
+
+	public void empUpdate(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		res.sendRedirect("di/empUpdate");
+	}
+
+	public void empDelete(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		res.sendRedirect("di/empDelete");
+	}
 }
--- spring4_1_1/src/kosmo80/mvc/sp4/EmpLogic.java
+++ spring4_1_1/src/kosmo80/mvc/sp4/EmpLogic.java
@@ -1,14 +1,21 @@
 package kosmo80.mvc.sp4;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
 
 public class EmpLogic {
-	Logger logger = Logger.getLogger(EmpLogic.class);
-	public List<Map<String, Object>> getEmpList(){
-		logger.info("getEmpList 호출 성공");
-		return null;
+	   Logger logger = Logger.getLogger(EmpLogic.class);
+
+	   public List<Map<String, Object>> getEmpList(List<Map<String, Object>> pList) {
+	      logger.info("getEmpList() called successfully");
+
+	      Map<String, Object> rmap = new HashMap<>();
+	      rmap.put("mem_name", "이순신");
+	      pList.add(rmap);
+
+	      return pList;
+	   }
 	}
-}
