3f1435ab34c5239442857d992257ce64a8aa9f2b
jeonjunhyeon <friendwnsgus@naver.com>
2021-05-14 16:54:56 +0900

    EmpController, EmpLogic 내용 추가

--- spring4_1_1/src/kosmo80/mvc/sp4/EmpController.java
+++ spring4_1_1/src/kosmo80/mvc/sp4/EmpController.java
@@ -1,8 +1,15 @@
 package kosmo80.mvc.sp4;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
@@ -10,7 +17,13 @@ public class EmpController extends MultiActionController {
 
 		public ModelAndView getEmpList(HttpServletRequest req
 										, HttpServletResponse res) {
+			Logger logger = Logger.getLogger(EmpController.class);
+			private EmpLogic empLogic = null;
+			//setter 객체 주입 코드
 			
+			 List<Map<String, Object>> empList = new ArrayList<>();
+			 Map<String, Object> rmap = new HashMap<>();
+			 rmap.put()
 			ModelAndView mav = new ModelAndView();                    
 			/*
 			 * <property name="prefix" value="/WEB-INF/views/"/> <property name="prefix"
